cff1e2f518f582d840006b84ed554f18afdd8ac4
ayarabia <ayarabia732@gmail.com>
2022-11-29 10:21:55 +0200

    add Cursor

--- src/App.js
+++ src/App.js
@@ -6,27 +6,13 @@ import NavBarCom from './components/navbar';
 import Stories from './components/stories';
 import WorkCard from './components/workCard';
 import Works from './components/works';
-import AnimatedCursor from "react-animated-cursor"
-import Cursor from "react-cursor-follow";
+
 function App() {
   return (
     <div >
-     <Cursor hollow color="#ee82ee" duration={0.8} size={45}  custom={false} className="tt"/>
+     {/* <Cursor hollow color="#ee82ee" duration={0.8} size={45}  custom={false} className="tt"/> */}
  
-  {/* <AnimatedCursor 
-    className="test"
-     color="193, 11, 111" 
-    outerStyle={{
-      // border: '3px solid red',
-     width:"50px",
-      height:"50px",
-      background:"transparant"
-     }}
-     innerSize={0}
-      outerSize={20}      
-      outerScale={1.7}
-    
-    />  */}
+  
   <NavBarCom></NavBarCom>
     <Counter></Counter>
     <Stories></Stories>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,13 +2,36 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { BsMoon } from 'react-icons/bs'
+ import React, {  useState } from "react";
 import  "./styles.css";
-
+import AnimatedCursor from "react-animated-cursor"
 function NavBarCom() {
+  const [border, setBorder] = useState("3px solid red");
+  const    [background ,setBackground] = useState("transparant");
   return (
 <>
-
-<Navbar  expand="lg" className='py-0 animat' >
+<AnimatedCursor 
+    className="test"
+    //  color={border}
+    outerStyle={{
+      border:`${border}`,
+     width:"50px",
+      height:"50px",
+      background:`${background}`
+     }}
+     innerSize={0}
+      outerSize={20}      
+      outerScale={1.7}
+    
+    />
+<Navbar  expand="lg" className='py-0 animat'  onMouseEnter={() => {
+          setBorder("0px solid #ff000052");
+          setBackground("#ff000052")
+        }}
+        onMouseLeave={() => {
+          setBackground("")
+          setBorder("3px solid red");
+        }}>
       <Container className='position-relative '>
  
    <div className='logo '>
@@ -21,8 +44,8 @@ function NavBarCom() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" >
         
-          <Nav className=' animate__animated animate__backInDown animate__delay-3s' >
-            <Nav.Link href="#deets" className=' p-4 '>WORK</Nav.Link>
+          <Nav className=' animate__animated animate__backInDown animate__delay-3s'  >
+            <Nav.Link href="#deets" className=' p-4 '   >WORK</Nav.Link>
             <Nav.Link href="#deets" className=' p-4'>ABOUT</Nav.Link>
             <Nav.Link href="#deets" className='p-4 '>STORIES</Nav.Link>
             <Nav.Link href="#deets" className='p-4 '>CONTACT</Nav.Link>
